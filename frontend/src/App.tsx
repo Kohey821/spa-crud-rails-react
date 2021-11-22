c73e0f3b77f2c97b887c43c88eb0aafcbb719ebd
kohey <very.tasty.konjac@gmail.com>
2021-11-22 16:50:20 +0900

    プレーンな投函を実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [image, setImage] = useState<File>();
+
+  async function handlePost() {
+    if (!(title && body && image)) return;
+
+    const { dir, log } = console;
+
+    const data = new FormData();
+    data.append('title', title);
+    data.append('body', body);
+    data.append('image', image);
+
+    try {
+      const response = await axios({
+        url: `${process.env.REACT_APP_API_URL}/posts`,
+        method: 'POST',
+        data,
+      });
+
+      log(response);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        log(error.response?.data);
+      } else {
+        dir(error);
+      }
+    }
+  }
+
   return (
     <>
-      基礎
+      <p>
+        タイトル:{' '}
+        <input
+          type="text"
+          name="title"
+          onChange={
+            (e) => setTitle(e.target.value)
+          }
+        />
+      </p>
+
+      <p>
+        本文:{' '}
+        <input
+          type="text"
+          name="body"
+          onChange={
+            (e) => setBody(e.target.value)
+          }
+        />
+      </p>
+
+      <p>
+        <input
+          type="file"
+          name="image"
+          id="image"
+          style={{
+            display: 'none',
+          }}
+          accept="image/*, .pdf"
+          onChange={
+            (e) => setImage(
+              e.target.files
+                ? e.target.files[0]
+                : undefined
+            )
+          }
+        />
+        <label htmlFor="image">画像を選択</label>
+      </p>
+
+      {image?.name && (
+        <p>
+          選択中の画像:{' '}
+          {image.name}
+        </p>
+      )}
+
+      <p>
+        <button
+          onClick={handlePost}
+        >
+          投函
+        </button>
+      </p>
     </>
   );
 }
