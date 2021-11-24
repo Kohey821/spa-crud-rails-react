f8d77f765487add95c743c9cb38c865875a4a00e
kohey <very.tasty.konjac@gmail.com>
2021-11-25 04:09:23 +0900

    詳細ページ　

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -1,14 +1,47 @@
+import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import Content from '../components/Content';
+import { Props as PostProps } from '../components/Post';
+import useAxios from '../hooks/useAxios';
+import {
+  Heading,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 
 export default function Detail() {
   const params = useParams();
+  const [post, setPost] = React.useState<PostProps>();
+  const [title, setTitle] = React.useState('詳細');
+  const { executeAxios } = useAxios();
+  React.useEffect(() => {
+    executeAxios<PostProps>({
+      method: 'GET',
+      url: `${process.env.REACT_APP_API_URL}/posts/${params.id}`,
+    }, ({ data }) => {
+      setPost(data);
+      setTitle(data.title);
+    });
+  }, [/* eslint-disable-line react-hooks/exhaustive-deps */])
 
   return (
     <Content
-      title="詳細"
+      title={title}
     >
-      {params.id}
+      {post && (
+        <>
+          {post.image_url && (
+            <Image
+              mt="2"
+              src={`${process.env.REACT_APP_API_URL_ROOT}${post.image_url}`}
+            />
+          )}
+
+          <Text as="p" mt="2">
+            {post.body}
+          </Text>
+        </>
+      )}
     </Content>
   );
 }
